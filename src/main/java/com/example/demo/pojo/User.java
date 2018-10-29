078837956ddf647fd95baeb62ef4a343a5bd1934
zhangyy <zhang10092009@hotmail.com>
2018-10-29 18:02:26 +0800

    添加正则校验

--- src/main/java/com/example/demo/pojo/User.java
+++ src/main/java/com/example/demo/pojo/User.java
@@ -7,6 +7,7 @@ import lombok.Data;
 
 import javax.annotation.sql.DataSourceDefinition;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 /**
  * @Auther: zhangyy
@@ -20,12 +21,16 @@ import javax.validation.constraints.NotNull;
 public class User {
     @ApiModelProperty(value = "主键id")
     private Long id;
+
     @NotNull(message = "请输入用户名")
     @ApiModelProperty(value = "姓名", name = "userName", example = "zhangsan", required = true)
     private String userName;
+
     @NotNull(message = "请输入年龄")
     @ApiModelProperty(value = "年龄", name = "age", example = "18", required = true)
+    @Pattern(regexp = "([0-9]|[0-9]{2}|120)", message = "年龄必须是小于120的正整数")
     private Integer age;
+
     @ApiModelProperty(value = "密码")
     private String password;
 }
